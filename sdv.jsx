31831a2dba5438c1110f9777edd09e9c8ddcb148
maia <mwerbos@gmail.com>
2024-03-15 16:34:24 -0400

    fix bug in checkbox storage

--- sdv.jsx
+++ sdv.jsx
@@ -2,16 +2,20 @@
 
 const e = React.createElement;
 
+function makeId(labelText) {
+  return labelText.toLowerCase().replace(/\s/g, '');
+}
+
 class CheckboxElement extends React.Component {
 	constructor(props) {
 		// this.localStoragePath (list of strings, at least 1 long)
 		// this.bgColor (string), this.fgColor (string)
 		// this.labelText (string)
 		super(props);
-    this.id = this.props.labelText.toLowerCase().replace(/\s/g, '');
+    this.id = makeId(this.props.labelText);
 		this.state = { checked: false };
 		let fromStorage = getFromStorage(this.props.localStoragePath);
-		if (!!fromStorage) {
+		if (fromStorage === true) {
 			this.state.checked = fromStorage;
 		}
 		this.onChange = this.onChange.bind(this);
@@ -48,10 +52,8 @@ class RecursiveCheckboxContainer extends React.Component {
 	constructor(props) {
 		// this.props.container
 		// this.props.localStoragePath
+		// this.fgColor, this.bgColor
 		super(props);
-		console.log('Creating recursive checkbox container');
-		console.log(this.props.container);
-		console.log(this.props.container);
 	}
 
 	render() {
@@ -60,15 +62,13 @@ class RecursiveCheckboxContainer extends React.Component {
 		return (<ul className={ulClassName}>
 			{this.props.container.map((item) => {
 				if (typeof item === 'string') {
-					// TODO: refactor so we don't repeat this inside CheckboxElement.
-					let storagePath = topLevelStoragePath.concat([item.toLowerCase().replace(/\s/g, '')]);
+					let storagePath = topLevelStoragePath.concat([makeId(item), 'checked']);
 					return <CheckboxElement labelText={item} localStoragePath={storagePath} fgColor={this.props.fgColor} bgColor={this.props.bgColor} />;
 				} else if (typeof item === 'object') {
-					// TODO: refactor so we don't repeat this inside CheckboxElement.
-					let headingId = item.heading.toLowerCase().replace(/\s/g, '');
+					let headingId = makeId(item.heading);
 					let storagePath = topLevelStoragePath.concat([headingId]);
 					return (<div className="recursive-div">
-						<CheckboxElement labelText={item.heading} localStoragePath={storagePath} fgColor={item.fgColor} bgColor={item.bgColor} />
+						<CheckboxElement labelText={item.heading} localStoragePath={storagePath.concat(['checked'])} fgColor={item.fgColor} bgColor={item.bgColor} />
 						<RecursiveCheckboxContainer container={item.items} localStoragePath={storagePath} fgColor={item.fgColor} bgColor={item.bgColor} />
 					</div>);
 				} else {
