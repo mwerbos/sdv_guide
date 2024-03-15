c9a773630c7cb9fae861d2eb30d1e67cc6bff852
maia <mwerbos@gmail.com>
2024-03-15 15:38:09 -0400

    CheckboxElement

--- sdv.jsx
+++ sdv.jsx
@@ -2,13 +2,58 @@
 
 const e = React.createElement;
 
+class CheckboxElement extends React.Component {
+	constructor(props) {
+		// this.localStoragePath (list of strings, at least 1 long)
+		// this.bgColor (string), this.fgColor (string)
+		// this.labelText (string)
+		super(props);
+    this.id = this.props.labelText.toLowerCase().replace(/\s/g, '');
+		this.state = { checked: false };
+		let fromStorage = getFromStorage(this.props.localStoragePath);
+		if (!!fromStorage) {
+			this.state.checked = fromStorage;
+		}
+		this.onChange = this.onChange.bind(this);
+	}
+
+	componentDidUpdate() {
+		putInStorage(this.props.localStoragePath, this.state.checked);
+	}
+
+	onChange(event) {
+		this.setState({ checked: event.target.checked });
+	}
+
+	render() {
+		return (<li
+			className="pretty-checkbox"
+			style={{
+				color: this.props.fgColor,
+				backgroundColor: this.props.bgColor,
+				borderColor: this.props.fgColor
+			}}>
+				<input type="checkbox"
+					id={this.id}
+					name={this.props.labelText}
+					onChange={this.onChange}
+					checked={this.state.checked}
+				></input>
+				<label htmlFor={this.id}>{this.props.labelText}</label>
+		</li>);
+	}
+}
+
 class MainContainer extends React.Component {
   constructor(props) {
     super(props);
 	}
 
 	render() {
-		return (<h1>Hello, World!! I am a React container!!</h1>);
+		return (<div>
+			<h1>Hello, World!! I am a React container!!</h1>
+			<CheckboxElement fgColor="#f00" bgColor="#000" labelText="Test Checkbox" localStoragePath={["Test Checkbox"]} />
+		</div>);
 	}
 }
 
