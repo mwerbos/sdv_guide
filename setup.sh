8d8ad95ad662b8e0b2e0f9876999856c5ed7ca72
maia <mwerbos@gmail.com>
2024-03-17 12:35:08 -0400

    add scripts

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# These are required for prettier.sh to work.
+sudo apt install npm
+# Node version manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+nvm install 14.0
